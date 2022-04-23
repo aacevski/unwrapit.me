28dc4d3ad20d6f996f787669d4e9fddf8cf507b8
aacevski <aacevski@gmail.com>
2022-04-23 22:32:20 +0200

    feat: adding confetti on generated playlist & disabling button

--- src/remotion/end-screen.tsx
+++ src/remotion/end-screen.tsx
@@ -1,5 +1,9 @@
-import { VStack, Heading, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { VStack, Heading, Button } from '@chakra-ui/react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
+
 import fetcher from '../utils/fetcher';
 
 type Props = {
@@ -7,6 +11,9 @@ type Props = {
 };
 
 const EndScreen = ({ trackUris }: Props) => {
+  const [isPlaylistGenerated, setIsPlaylistGenerated] = useState(false);
+  const { width, height } = useWindowSize();
+
   const { refetch } = useQuery(
     'create-playlist',
     () =>
@@ -26,31 +33,36 @@ const EndScreen = ({ trackUris }: Props) => {
 
   const onClick = () => {
     refetch();
+    setIsPlaylistGenerated(true);
   };
 
   return (
-    <VStack
-      w="full"
-      h="full"
-      align="center"
-      justify="center"
-      bg="linear-gradient(to right, #0f0c29, #302b63, #24243e)"
-      py={20}
-      px={10}
-      spacing={10}
-      rounded="lg"
-    >
-      <Heading>Were you happy with the results?</Heading>
-      <Button
-        width={80}
-        height={20}
-        fontSize="2xl"
-        variant="solid"
-        onClick={onClick}
+    <>
+      <VStack
+        w="full"
+        h="full"
+        align="center"
+        justify="center"
+        bg="linear-gradient(to right, #0f0c29, #302b63, #24243e)"
+        py={20}
+        px={10}
+        spacing={10}
+        rounded="lg"
       >
-        Generate playlist
-      </Button>
-    </VStack>
+        <Heading>Were you happy with the results?</Heading>
+        <Button
+          px={10}
+          height={28}
+          fontSize="4xl"
+          variant="solid"
+          onClick={onClick}
+          disabled={isPlaylistGenerated}
+        >
+          {isPlaylistGenerated ? 'Playlist generated' : 'Generate playlist'}
+        </Button>
+      </VStack>
+      {isPlaylistGenerated && <Confetti width={width} height={height} />}
+    </>
   );
 };
 
