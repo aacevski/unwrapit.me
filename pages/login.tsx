d5738d1c57390f94eade2f72283fceb46cd32483
aacevski <aacevski@gmail.com>
2022-04-24 00:33:43 +0200

    feat: adding better login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,52 @@
-import { VStack, Button, Icon } from '@chakra-ui/react';
-import { signIn } from 'next-auth/react';
-import { BsSpotify } from 'react-icons/bs';
+import { VStack, Heading, Link, Text } from '@chakra-ui/react';
 
+import { signIn } from 'next-auth/react';
 import { CALLBACK_URL } from '../src/constants/urls';
 
 const Login = () => {
   return (
-    <VStack align="center" justify="center" w="full">
-      <Button
-        onClick={() => signIn('spotify', { callbackUrl: CALLBACK_URL })}
-        leftIcon={<Icon as={BsSpotify} />}
-      >
-        sign in
-      </Button>
+    <VStack
+      align="center"
+      justify="center"
+      w="full"
+      h="full"
+      flex={1}
+      spacing={10}
+      textAlign="center"
+    >
+      <VStack>
+        <Heading size="lg">who?</Heading>
+        <Heading size="md" fontWeight="light">
+          <Link
+            isExternal
+            href="https://github.com/aacevski/unwrapit.me"
+            target="_blank"
+          >
+            <strong>unwrapped.me</strong>
+          </Link>{' '}
+          is an open source project
+        </Heading>
+      </VStack>
+      <VStack>
+        <Heading>what?</Heading>
+        <Heading size="md" fontWeight="light">
+          it generates a video based on your most listened artists / tracks
+        </Heading>
+      </VStack>
+      <VStack>
+        <Heading size="lg">how?</Heading>
+        <Heading size="md" fontWeight="light">
+          to proceed, please{' '}
+          <Text
+            as="span"
+            decoration="underline"
+            cursor="pointer"
+            onClick={() => signIn('spotify', { callbackUrl: CALLBACK_URL })}
+          >
+            sign in
+          </Text>
+        </Heading>
+      </VStack>
     </VStack>
   );
 };
