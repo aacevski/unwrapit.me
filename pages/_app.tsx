d07577eac0fc9fbac5672f80b6a0977d664d1bdd
aacevski <aacevski@gmail.com>
2022-05-03 00:28:19 +0200

    feat: adding better SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { NextSeo } from 'next-seo';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { SessionProvider } from 'next-auth/react';
@@ -12,12 +13,32 @@ import '../style.css';
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <SessionProvider session={session}>
         <ChakraProvider theme={theme}>
+          <NextSeo
+            title="unwrapit"
+            description="Have you ever wanted to see your most listened artists and tracks anytime of the year? with unwrapit.me you can!"
+            openGraph={{
+              url: 'https://unwrapit.vercel.app',
+              title: 'unwrapit',
+              description:
+                'Have you ever wanted to see your most listened artists and tracks anytime of the year? with unwrapit.me you can!',
+              locale: 'en_US',
+              images: [
+                {
+                  url: 'https://unwrapit.vercel.app/assets/images/social.png',
+                  width: 1200,
+                  height: 630,
+                  alt: 'unwrapit',
+                  type: 'image/png',
+                },
+              ],
+            }}
+          />
           <UserProvider>
             <Layout>
               <Component {...pageProps} />
@@ -29,4 +50,5 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   );
 }
 
-export default MyApp;
+export default App;
+
